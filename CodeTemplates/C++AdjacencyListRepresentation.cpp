38683872c76050f7190e7f54b7dc70b4a26c7f27
Kusum <72653558+kusumpriya19@users.noreply.github.com>
2021-07-26 01:21:48 +0530

    Add files via upload

---
+++ CodeTemplates/C++AdjacencyListRepresentation.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+int main() {
+	ll n, m;
+	cin >> n >> m; 
+	
+	// declare the adjacent matrix 
+	vector<int> adj[n+1]; 
+	
+	// take edges as input 
+	for(ll i = 0;i<m;i++) {
+	    int u, v; 
+	    cin >> u >> v;
+	    adj[u].push_back(v); 
+	    adj[v].push_back(u); 
+	}
+	return 0;
+}
